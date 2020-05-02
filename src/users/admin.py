157e4dba41e6ff1419383fb12733e35697ef332b
Pierre Robenson <robenson1.pierre@epitech.eu>
2020-05-02 12:07:56 +0200

    register user abstract

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from users.models import Profile
+from django.contrib.auth.models import Group
+from users.models import User, Profile
 
+admin.site.unregister(Group)
+admin.site.register(User)
 admin.site.register(Profile)
