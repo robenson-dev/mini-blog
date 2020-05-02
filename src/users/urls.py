2b4081281b613ab1db44df98fb9f9a276141ca12
Pierre Robenson <robenson1.pierre@epitech.eu>
2020-05-02 02:22:50 +0200

    route pour les vue

---
+++ src/users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from users import views
+
+
+app_name = 'users'
+urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
+    path('profile/', views.profile, name='profile'),
+
+
+]
