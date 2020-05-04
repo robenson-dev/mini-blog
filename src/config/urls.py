433cdd6dacd27d58f6c33ac7feb1e2031518df66
Pierre Robenson <robenson1.pierre@epitech.eu>
2020-05-04 20:47:34 +0200

    ckeditor route

--- src/config/urls.py
+++ src/config/urls.py
@@ -8,9 +8,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('secret/', admin.site.urls),
-    path('user/', include('users.urls')),
     path('', include('blogger.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 
+    path('user/', include('users.urls')),
     path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
