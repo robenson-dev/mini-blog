efa2fd9e4d5d3d9390c962dd340acddfa0a0d3e5
Pierre Robenson <robenson1.pierre@epitech.eu>
2020-05-04 20:36:06 +0200

    change route default du decorateur
    - login_url

--- src/users/decorators.py
+++ src/users/decorators.py
@@ -2,7 +2,7 @@ from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.auth.decorators import user_passes_test
 
 
-def super_blogger_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
+def super_blogger_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='users:signin'):
 
     actual_decorator = user_passes_test(
         lambda u: u.is_active and u.is_super_blogger,
