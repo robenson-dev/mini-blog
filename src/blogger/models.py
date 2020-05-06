b2fd824d5109392c0b98f125cc3cf9655661d1e4
Pierre Robenson <robenson1.pierre@epitech.eu>
2020-05-06 05:23:03 +0200

    creation comment models

--- src/blogger/models.py
+++ src/blogger/models.py
@@ -3,12 +3,13 @@ from django.utils import timezone
 from users.models import User
 from django.urls import reverse
 
-from ckeditor_uploader.fields import RichTextUploadingField
+from django_ckeditor_5.fields import CKEditor5Field
+
 
 class Post(models.Model):
 
     title = models.CharField(max_length=100)
-    content = RichTextUploadingField(blank=True, null=True)
+    content = CKEditor5Field(config_name='extends', blank=True, null=True)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -17,3 +18,14 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail-post', kwargs={'pk': self.pk})
+
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = CKEditor5Field(config_name='extends', blank=True, null=True)
+    release_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.post.title}-{self.user.username}'
