48e1c938eedc95aa29ffa42a40ce6eef7fd7aebe
Pierre Robenson <robenson1.pierre@epitech.eu>
2020-05-06 05:22:42 +0200

    add views
    - delete = post
    - detail = post
    - posts_all

--- src/blogger/urls.py
+++ src/blogger/urls.py
@@ -5,6 +5,8 @@ from blogger.views import blogger, member, legendary
 # app_name = 'blogger'
 urlpatterns = [
     path('', blogger.home, name='home'),
+    path('posts/', legendary.PostListView.as_view(), name='post'),
+    path('post/<int:pk>', legendary.PostDetailView.as_view(), name='detail-post'),
 
     path('member/', include(([
         path('', member.details, name='member-detail'),
@@ -12,9 +14,9 @@ urlpatterns = [
 
     path('legendary/', include(([
         path('', legendary.dashboard, name='legendary-dashboard'),
-        path('post/<int:pk>', legendary.PostDetailView.as_view(), name='detail-post'),
         path('post/new', legendary.PostCreateView.as_view(), name='create-post'),
         path('post/<int:pk>/update/', legendary.PostUpdateView.as_view(), name='update-post'),
+        path('post/<int:pk>/delete/', legendary.PostDeleteView.as_view(), name='delete-post'),
     ], 'blogger'), namespace='legendary')),
 
 ]
